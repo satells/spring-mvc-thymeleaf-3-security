0688c82f28ba45a2e77b4c9bde0555bdb5389475
satells <msergiost@hotmail.com>
2021-12-15 16:27:25 -0300

    Start.

---
+++ asecurity/src/main/java/com/asecurity/AsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.asecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsecurityApplication.class, args);
+	}
+
+}
---
+++ asecurity/src/test/java/com/asecurity/AsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
