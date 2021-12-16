338e8835973401111581487e8a044d1f9b0050de
satells <msergiost@hotmail.com>
2021-12-16 16:14:23 -0300

    .

---
+++ asecurity/src/main/java/com/asecurity/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.asecurity.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/", "/home", "/webjars/**", "/css/**", "/js/**", "/image/**").permitAll()
+
+				.anyRequest().authenticated()
+
+				.and()
+
+				.formLogin().loginPage("/login").defaultSuccessUrl("/", true).failureUrl("/login-error").permitAll().
+
+				and().logout().logoutSuccessUrl("/");
+	}
+
+}
--- asecurity/src/main/java/com/asecurity/web/controller/HomeController.java
+++ asecurity/src/main/java/com/asecurity/web/controller/HomeController.java
@@ -1,14 +1,33 @@
 package com.asecurity.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
 	// abrir pagina home
-	@GetMapping("/")
+	@GetMapping({ "/", "/home" })
 	public String home() {
 		return "home";
-	}	
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	@GetMapping("/login-error")
+	public String loginError(ModelMap model) {
+
+		model.addAttribute("alerta", "erro");
+		model.addAttribute("titulo", "Credenciais inválidas.");
+		model.addAttribute("texto", "Login ou senha incorretos.");
+		model.addAttribute("subtexto", "Acesso permitido apenas para cadastros já ativados.");
+
+		return "login";
+
+	}
+
 }
