f1bd5b7373b7c8ba68db4917b8ccec5f3df6f47c
satells <msergiost@hotmail.com>
2022-01-05 15:58:57 -0300

    .

--- asecurity/src/main/java/com/asecurity/AsecurityApplication.java
+++ asecurity/src/main/java/com/asecurity/AsecurityApplication.java
@@ -4,12 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AsecurityApplication {
+public class AsecurityApplication {// implements CommandLineRunner {
 
 	public static void main(String[] args) {
 //		System.out.println(new BCryptPasswordEncoder().encode("123"));
 
 		SpringApplication.run(AsecurityApplication.class, args);
 	}
+
+//	@Autowired
+//	private JavaMailSender sender;
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//		SimpleMailMessage mail = new SimpleMailMessage();
+//		mail.setFrom("emailchecktask@gmail.com");
+//		mail.setTo("msergiost@hotmail.com");
+//		mail.setText("tested,.........");
+//		mail.setSubject("teste 3");
+//
+//		sender.send(mail);
+//
+//	}
 
 }
